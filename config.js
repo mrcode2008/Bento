6896475483d35241c33ad7efdf2f125d6d25346c
mrcode2008 <95929041+mrcode2008@users.noreply.github.com>
2023-02-22 12:21:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'mrcode2008',
 	imageBackground: false,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'Good morning!',
